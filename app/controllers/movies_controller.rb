a7ff1b800f877c35a5452e8729624b55e3f3206b
Shivraj <itscool.sid@gmail.com>
2015-12-20 12:29:44 +0530

    First commit

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,7 +3,7 @@ class MoviesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   
   def index
-    @movies = Movie.all
+    @movies = Movie.where(["title LIKE ?","%#{params[:search]}%"])
   end
 
   
